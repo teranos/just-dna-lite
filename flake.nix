ee859e98314d0a8e708f83fee7488424351effd0
Brandon van Houten <brandon.vhouten@gmail.com>
2026-03-21 01:15:57 +0100

    Fix Nix CI: single shell session, add libstdc++ for Linux

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        isLinux = pkgs.stdenv.hostPlatform.isLinux;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -17,10 +18,14 @@
             pkgs.python313
             pkgs.uv
             pkgs.nodejs_22
+          ] ++ pkgs.lib.optionals isLinux [
+            pkgs.stdenv.cc.cc.lib
           ];
 
           shellHook = ''
             export UV_PYTHON="${pkgs.python313}/bin/python3"
+          '' + pkgs.lib.optionalString isLinux ''
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
           '';
         };
       });
